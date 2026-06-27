3e84536bdd1351853b67c264ab4b4f74ed9a3926
BRBX <egorka2005sautkin@gmail.com>
2026-06-27 16:09:05 +0300

    Add parameters to BrbxText.kt

--- ui-compose/src/main/java/com/brbx/ui_compose/common/BrbxText.kt
+++ ui-compose/src/main/java/com/brbx/ui_compose/common/BrbxText.kt
@@ -17,13 +17,34 @@ import androidx.compose.ui.res.stringResource
 sealed interface BrbxText {
 
     @JvmInline
-    value class Res(@param:StringRes val resId: Int) : BrbxText
-
-    @JvmInline
     value class Raw(val text: String) : BrbxText
+
+    class Res(
+        @param:StringRes val resId: Int,
+        vararg val formatArgs: Any,
+    ) : BrbxText {
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Res
+            if (resId != other.resId) return false
+            if (!formatArgs.contentEquals(other.formatArgs)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = resId
+            result = 31 * result + formatArgs.contentHashCode()
+            return result
+        }
+    }
 }
 
-fun @receiver:StringRes Int.toBrbxText(): BrbxText.Res = BrbxText.Res(resId = this)
+fun @receiver:StringRes Int.toBrbxText(vararg formatArgs: Any): BrbxText.Res =
+    BrbxText.Res(resId = this, *formatArgs)
 
 fun String.toBrbxText(): BrbxText.Raw = BrbxText.Raw(text = this)
 
@@ -31,11 +52,11 @@ fun String.toBrbxText(): BrbxText.Raw = BrbxText.Raw(text = this)
 fun BrbxText.asString(): String =
     when (this) {
         is BrbxText.Raw -> this.text
-        is BrbxText.Res -> stringResource(id = this.resId)
+        is BrbxText.Res -> stringResource(id = this.resId, formatArgs = this.formatArgs)
     }
 
 fun BrbxText.asString(context: Context): String =
     when (this) {
         is BrbxText.Raw -> this.text
-        is BrbxText.Res -> context.getString(this.resId)
+        is BrbxText.Res -> context.getString(this.resId, *this.formatArgs)
     }
