fdaeaf02d1179872b75064ae31fc452c9d664f78
BRBX <egorka2005sautkin@gmail.com>
2026-06-29 14:15:16 +0300

    Add brbxScrollDirection to lazyGridState

--- ui-compose/src/main/java/com/brbx/ui_compose/state/brbxScrollDirection.kt
+++ ui-compose/src/main/java/com/brbx/ui_compose/state/brbxScrollDirection.kt
@@ -1,6 +1,7 @@
 package com.brbx.ui_compose.state
 
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
@@ -11,7 +12,19 @@ enum class BrbxScrollDirection {
 
 @Composable
 fun LazyListState.brbxScrollDirection(): BrbxScrollDirection {
-    val direction = remember {
+    val direction = remember(key1 = this) {
+        derivedStateOf {
+            val isScrollingUp = (firstVisibleItemIndex == 0 && firstVisibleItemScrollOffset == 0) ||
+                    lastScrolledBackward || !canScrollForward
+            if (isScrollingUp) BrbxScrollDirection.Up else BrbxScrollDirection.Down
+        }
+    }.value
+    return direction
+}
+
+@Composable
+fun LazyGridState.brbxScrollDirection(): BrbxScrollDirection {
+    val direction = remember(key1 = this) {
         derivedStateOf {
             val isScrollingUp = (firstVisibleItemIndex == 0 && firstVisibleItemScrollOffset == 0) ||
                     lastScrolledBackward || !canScrollForward
